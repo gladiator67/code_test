21eb4249c4c24a110bb16cc9ee713c215519184d
gladiator67 <gladiator67@naver.com>
2025-01-28 10:31:48 +0900

    Update Main_12.java

--- linear/Main_12.java
+++ linear/Main_12.java
@@ -11,20 +11,20 @@ public class Main_12 {
         int[] answer = new int[n];
 
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        stack.push(0);  // 인덱스를 저장. 여기서 인덱스는 시간을 의미. 초기값으로  0 을 지정
 
         for(int i=1 ; i< n ; i++) {
             while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 int j = stack.pop();
                 answer[j] = i-j;
-            }
+            }   // Stack에 저장하다가 값이 하강하면 Stack에서 그 인덱스를 제거하고 answer 배열에 저장
 
-            stack.push(i);
+            stack.push(i);  // 값이 상승하면 Stack에 값을 저장
         }
 
-        while(!stack.isEmpty()){
+        while(!stack.isEmpty()){   // Stack에 저장된 값을 answer 배열에 불어와 저장 
             int j = stack.pop();
-            answer[j] = n-j-1;
+            answer[j] = n-j-1;  // j는 0부터 시작됨
         }
                 
         return answer;
