3032d579790f01d04d93e857d6a0c2939c91b2ff
gladiator67 <gladiator67@naver.com>
2025-01-28 10:25:33 +0900

    Update Main_10.java

--- linear/Main_10.java
+++ linear/Main_10.java
@@ -6,7 +6,7 @@ public class Main_10 {
 
     // "[](){}"   -->	3
 
-    private static String shift(String s) {
+    private static String shift(String s) {    // 입력 문자를 회전하는 함수
         String temp = s.substring(1);
         Character first = s.charAt(0);
 
@@ -15,7 +15,7 @@ public class Main_10 {
         return temp + first;
     }
 
-    private static boolean verificate(String str) {
+    private static boolean verificate(String str) {    // Stack을 사용하여 같은 쌍의 괄호를 제거
         Stack<Character> stack = new Stack<>();
 
         for(int i = 0 ; i < str.length() ; i++) {
