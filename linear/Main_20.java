caeef6a562d81a07de363fa8cf64b3e5cde9cb01
gladiator67 <gladiator67@naver.com>
2025-02-02 08:42:30 +0900

    Update Main_20.java

--- linear/Main_20.java
+++ linear/Main_20.java
@@ -5,16 +5,17 @@ import java.util.Map;
 
 public class Main_20 {
 
-    public static int solution(String[] want, 
-    							int[] number, String[] discount) {
+    public static int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
+
+        // 구입하고자 하는 물품을 Map으로 생성
         Map<String, Integer> wantMap = new HashMap<>();
         for(int i=0 ; i< want.length ; i++) {
             wantMap.put(want[i], number[i]);
         }
 
         for(int i=0 ; i<discount.length - 10 + 1 ; i++) {
+            // 10일동안 할인하는 물품을 Map으로 생성
             Map<String, Integer> discount10d = new HashMap<>();
 
             for(int j=i ; j<i+10 ; j++) {
@@ -24,6 +25,7 @@ public class Main_20 {
                 }
             }
 
+            // 구입하고자 하는 Map과 10일 동안 할인하는 Map이 일치하는지 확인
             if(discount10d.equals(wantMap)) {
                 answer++;
             }
