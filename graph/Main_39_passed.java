63595240cd6922c5e378ecfdc6dfcf37672d6d2b
gladiator67 <gladiator67@naver.com>
2025-02-19 11:01:37 +0900

    Update Main_39_passed.java

--- graph/Main_39_passed.java
+++ graph/Main_39_passed.java
@@ -4,11 +4,16 @@ import java.util.Queue;
 import java.util.ArrayDeque;
 
 public class Main_39_passed {
+    //기본 step을 지정한다.
     public static int[] dx = {0,0,1,-1};
     public static int[] dy = {1,-1,0,0};
-    public static int N, M;
-    public static int[][] visited;
     
+    // 지도의 기본 크기를 지정한다.
+    public static int N, M;
+    // 각 Node에 방문여부를 지정한다.
+    public static int[][] visited;
+
+    // 지도상의 Node를 지정할 객체 생성
     public static class Node{
         int y, x;
         public Node(int y, int x){
@@ -18,8 +23,11 @@ public class Main_39_passed {
     }
     
     public static int bfs(char start, char end, String[] maps){
+        //  깊이 우선 탐색을 위한 Queue 생성
         Queue<Node> queue = new ArrayDeque<>();
         visited = new int[N][M];
+        
+        // start 점일 경우 깊이 우선 탐색을 시작한다.
         for (int y = 0 ; y < N; y++){
             for (int x = 0; x < M; x++){
                 if (maps[y].charAt(x) == start){
@@ -34,23 +42,29 @@ public class Main_39_passed {
             int y = p.y;
             int x = p.x;
             
+            // 끝점일 경우 계산을 종료한다.
             if (maps[y].charAt(x) == end){
                 return visited[y][x] - 1;
             }
-            
+
+            // for loop를 사용하여 각 step을 이동한다.
             for (int i = 0 ; i < 4; i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                
+
+                // 지도의 외곽을 벗어나는 경우 계산을 생략한다.
                 if(nx < 0 || ny <0 || nx >= M || ny >= N )
                     continue;
 
+                // 이미 통과한 점일 경우 계산을 생략한다.
                 if(visited[ny][nx] > 0)
                     continue;
 
+                // 도달한 점인 벽인 경우 계산을 생략한다.
                 if(maps[ny].charAt(nx) == 'X')
                     continue;
 
+                // 방문 자료구조를 갱신하고 queue를 update한다.
                 visited[ny][nx] = visited[y][x] + 1;
                 queue.add(new Node(ny, nx));
             }
