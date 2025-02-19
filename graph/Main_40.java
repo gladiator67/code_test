7c9ad3b1e3693cb564ea5417662a623ce4da3233
gladiator67 <gladiator67@naver.com>
2025-02-19 12:37:14 +0900

    Update Main_40.java

--- graph/Main_40.java
+++ graph/Main_40.java
@@ -18,30 +18,30 @@ public class Main_40 {
     }
 
     public static int solution(int N, int[][] road, int K) {
-        List<Node>[] adjList = new ArrayList[N+1];
-        for(int i=1 ; i<=N ; i++) {
-            adjList[i] = new ArrayList<>();
+        List<List<Node>> adjList = new ArrayList<>();
+        for(int i=0; i<=N ; i++) {
+            adjList.add(new ArrayList<>());
         }
-        
-        for(int[] edge : road) {
-            adjList[edge[0]].add(new Node(edge[1], edge[2]));
-            adjList[edge[1]].add(new Node(edge[0], edge[2]));
-        }
-        
-        int[] dist = new int[N+1];
 
+        for(int[] edge : road) {
+            adjList.get(edge[0]).add(new Node(edge[1], edge[2]));
+            adjList.get(edge[1]).add(new Node(edge[0], edge[2]));
+        }
+
+        int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+
         Queue<Node> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-        queue.add(new Node(1,0));
+        queue.add(new Node(1, 0));
         dist[1] = 0;
 
         while(!queue.isEmpty()) {
             Node now = queue.poll();
 
-            if(dist[now.dest] < now.cost) 
+            if(dist[now.dest] < now.cost)
                 continue;
-
-            for(Node next : adjList[now.dest]) {
+            
+            for(Node next : adjList.get(now.dest)) {
                 if(dist[next.dest] > now.cost + next.cost) {
                     dist[next.dest] = now.cost + next.cost;
                     queue.add(new Node(next.dest, dist[next.dest]));
@@ -51,9 +51,11 @@ public class Main_40 {
 
         int answer = 0;
 
-        for( int i=1 ; i<=N ; i++) {
-            if(dist[i] <= K) answer++;
+        for(int i=1 ; i<=N ; i++) {
+            if(dist[i] <= K)
+                answer++;
         }
+
         return answer;
     }
 
