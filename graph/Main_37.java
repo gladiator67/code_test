323ded29d1bbc89d18ca69d4fccd306cda9ef61b
gladiator67 <gladiator67@naver.com>
2025-02-19 10:53:25 +0900

    Update Main_37.java

--- graph/Main_37.java
+++ graph/Main_37.java
@@ -5,9 +5,11 @@ import java.util.ArrayDeque;
 
 public class Main_37 {
 
+    // 기본 step을 지정한다.
     private static final int[] rx = {0,0,1,-1};
     private static final int[] ry = {1,-1,0,0};
 
+    // 지도의 위치를 저장하는 객체 생성
     private static class Node {
         int row,col;
 
@@ -18,27 +20,30 @@ public class Main_37 {
     }
     
     public static int solution(int[][] maps) {
-        int N = maps.length;
-        int M = maps[0].length;
+        int N = maps.length;  // 지도의 y 값 크기
+        int M = maps[0].length;  // 지도의 x 값 크기
         int[][] dist = new int[N][M];
 
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.add(new Node(0,0));
+        Queue<Node> queue = new ArrayDeque<>();  // Queue 생성
+        queue.add(new Node(0,0));  // 넓이 우선 탐색을 시작한다.
         dist[0][0] = 1;
 
         while(!queue.isEmpty()) {
             Node now = queue.poll();
 
             for(int i=0; i<4; i++) {
-                int nrow = now.row + ry[i];
-                int ncol = now.col + rx[i];
+                int nrow = now.row + ry[i];  // 각각의 step 배열을 이용하여 한 step씩 이동
+                int ncol = now.col + rx[i];  // 각각의 step 배열을 이용하여 한 step씩 이동
 
+                // 지도의 벽을 넘어서면 계산을 생략한다.
                 if(nrow < 0 || ncol < 0 || nrow >= N || ncol >= M) 
                     continue;
 
+                // 지도의 벽을 통과하면 계산을 생략한다.
                 if(maps[nrow][ncol] == 0)
                     continue;
 
+                // 노드가 처음 방문하는 경우, queue에 추가하고 거리를 갱신한다.
                 if(dist[nrow][ncol] == 0) {
                     queue.add(new Node(nrow,ncol));
                     dist[nrow][ncol] = dist[now.row][now.col] + 1;
@@ -46,6 +51,7 @@ public class Main_37 {
             }
         }
 
+        // 최단거리를 반환한다. 목표에 도달하지 못한 경우 -1 반환
         return dist[N-1][M-1] == 0 ? -1 : dist[N-1][M-1];
     }
 
