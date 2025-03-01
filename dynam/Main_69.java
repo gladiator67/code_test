eaa0642d9d87ec3ad85ea3ef6c9d158dbc7d7ef2
gladiator67 <gladiator67@naver.com>
2025-03-01 11:53:15 +0900

    Update Main_69.java

--- dynam/Main_69.java
+++ dynam/Main_69.java
@@ -6,21 +6,21 @@ public class Main_69 {
         int N = arr[0].length;
         int[][] dp = new int[4][N];
 
-        dp[0][0] = arr[0][0];
-        dp[1][0] = arr[1][0];
-        dp[2][0] = arr[2][0];
-        dp[3][0] = arr[0][0] + arr[2][0];
+        dp[0][0] = arr[0][0];    // pattern 0
+        dp[1][0] = arr[1][0];    // pattern 1
+        dp[2][0] = arr[2][0];    // pattern 2
+        dp[3][0] = arr[0][0] + arr[2][0];    // pattern 3
 
         for(int i=1 ; i<N ; i++) {
-            dp[0][i] = arr[0][i] + Math.max(dp[1][i-1], dp[2][i-1]);
+            dp[0][i] = arr[0][i] + Math.max(dp[1][i-1], dp[2][i-1]);   // pattern 0
             dp[1][i] = arr[1][i] + Math.max(dp[0][i-1], 
-            				Math.max(dp[2][i-1], dp[3][i-1]));
-            dp[2][i] = arr[2][i] + Math.max(dp[0][i-1], dp[1][i-1]);
-            dp[3][i] = arr[0][i] + arr[2][i] + dp[1][i-1];
+            				Math.max(dp[2][i-1], dp[3][i-1]));    // pattern 1
+            dp[2][i] = arr[2][i] + Math.max(dp[0][i-1], dp[1][i-1]);   // pattenr 2
+            dp[3][i] = arr[0][i] + arr[2][i] + dp[1][i-1];   // pattern 3
         }
 
         return Math.max(Math.max(dp[0][N-1], dp[1][N-1]), 
-        					Math.max(dp[2][N-1], dp[3][N-1]));
+        			Math.max(dp[2][N-1], dp[3][N-1]))  // 4개의 경우 중 가장 큰 경우 반환
     }
     
     public static void main(String[] args) {
