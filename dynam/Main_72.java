5798beb876eebcde41b0ff23730aa0cf75b86065
gladiator67 <gladiator67@naver.com>
2025-03-01 11:56:27 +0900

    Update Main_72.java

--- dynam/Main_72.java
+++ dynam/Main_72.java
@@ -11,8 +11,7 @@ public class Main_72 {
             dp[i][0] = triangle[i][0] + dp[i-1][0];
 
             for(int j=1 ; j<=i ;j++) {
-                dp[i][j] = triangle[i][j] + 
-                				Math.max(dp[i-1][j-1], dp[i-1][j]);
+                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]);
             }
 
             dp[i][i] = triangle[i][i] + dp[i-1][i-1];
@@ -28,8 +27,7 @@ public class Main_72 {
     }
 
     public static void main(String[] args) {
-        int[][] triangle = {{7}, {3,8}, {8,1,0}, 
-        								{2,7,4,4}, {4,5,2,6,5}};
+        int[][] triangle = {{7}, {3,8}, {8,1,0}, {2,7,4,4}, {4,5,2,6,5}};
         
         System.out.println(solution(triangle));
     }
